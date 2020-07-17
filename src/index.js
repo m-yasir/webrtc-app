10d54bcd026b7cb45693e54372e6e6414e7d32bd
Yasir Nadeem <m-yasir.dev@protonmail.com>
2020-07-17 15:27:46 +0500

    Update UI and styles

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ const { Server } = require("./server");
 const server = new Server();
 
 server.listen((port) => {
-    console.log(`Now listening on port number: ${port}...`);
+    console.log(`Now listening on port number: ${port}`);
 })
